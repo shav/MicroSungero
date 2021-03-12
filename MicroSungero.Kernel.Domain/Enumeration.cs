06482cd27495003ca58822da05cc4e5fecf74965
Шумков Артём (Shumkov_AV) <Shumkov_AV@directum.ru>
2021-03-13 01:56:37 +0400

    Additional info for enumeration

--- MicroSungero.Kernel.Domain/Enumeration.cs
+++ MicroSungero.Kernel.Domain/Enumeration.cs
@@ -6,9 +6,13 @@ using System.Reflection;
 namespace MicroSungero.Kernel.Domain
 {
   /// <summary>
-  /// An item of complete ended set of items.
-  /// Also used as a container for the set of enumerated items.
+  /// An named item of complete ended set of items.
+  /// Also used as a container for the set of named enumerated items.
   /// </summary>
+  /// <remarks>
+  /// See also for more details:
+  /// https://en.wikipedia.org/wiki/Enumerated_type
+  /// </remarks>
   public class Enumeration : IComparable
   {
     #region Properties and fields
