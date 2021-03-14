e00918cd7fc75908ed2307d582a7ecbbd369192f
Шумков Артём (Shumkov_AV) <Shumkov_AV@directum.ru>
2021-03-14 16:00:30 +0400

    Base exception type for domain exceptions.

---
+++ Kernel/Domain/MicroSungero.Kernel.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MicroSungero.Kernel.Domain.Exceptions
+{
+  /// <summary>
+  /// Base exception type for domain exceptions.
+  /// </summary>
+  public class DomainException : Exception
+  {
+    /// <summary>
+    /// Create domain exception.
+    /// </summary>
+    public DomainException()
+    {
+    }
+
+    /// <summary>
+    /// Create domain exception.
+    /// </summary>
+    /// <param name="message">Exception message.</param>
+    public DomainException(string message)
+        : base(message)
+    {
+    }
+
+    /// <summary>
+    /// Create domain exception.
+    /// </summary>
+    /// <param name="message">Exception message.</param>
+    /// <param name="innerException">Original system exception.</param>
+    public DomainException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+  }
+}
