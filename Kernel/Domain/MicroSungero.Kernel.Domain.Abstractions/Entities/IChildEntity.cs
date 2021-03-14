23914af264c58c64e3074e9f4a6a1e650518ba29
Шумков Артём (Shumkov_AV) <Shumkov_AV@directum.ru>
2021-03-15 00:47:05 +0400

    Child entity interface

---
+++ Kernel/Domain/MicroSungero.Kernel.Domain.Abstractions/Entities/IChildEntity.cs
@@ -0,0 +1,29 @@
+﻿namespace MicroSungero.Kernel.Domain.Entities
+{
+  /// <summary>
+  /// The entity whose lifecycle is entirely dependent on another entity (which is called root or parent entity).
+  /// </summary>
+  /// <remarks>
+  /// It’s a strong relationship: the Child Entity has only meaning in the context of its parent, and cannot exist outside of it.
+  /// If the Parent Entity ceases to exist, the Child Entity is deleted as well.
+  /// Ideally, all operations on the child are handled by the parent, even its creation.
+  /// </remarks>
+  public interface IChildEntity : IEntity
+  {
+    /// <summary>
+    /// Parent entity.
+    /// </summary>
+    IEntity RootEntity { get; }
+  }
+
+  /// <summary>
+  /// An internal interface of child entity that provides extended access.
+  /// </summary>
+  public interface IInternalChildEntity : IChildEntity
+  {
+    /// <summary>
+    /// Parent entity (allows to set parent entity).
+    /// </summary>
+    IEntity RootEntity { get; set; }
+  }
+}
