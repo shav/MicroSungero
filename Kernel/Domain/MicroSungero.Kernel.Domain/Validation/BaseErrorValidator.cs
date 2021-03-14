2f05e524878afcdb9f639637b7509112e9a3b5f2
Шумков Артём (Shumkov_AV) <Shumkov_AV@directum.ru>
2021-03-14 16:32:10 +0400

    Base error validator

---
+++ Kernel/Domain/MicroSungero.Kernel.Domain/Validation/BaseErrorValidator.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace MicroSungero.Kernel.Domain.Validation
+{
+  /// <summary>
+  /// Base class for object validation that can throw validation exceptions.
+  /// </summary>
+  /// <typeparam name="T">Type of objects that are validated by particular implementation of base validator.</typeparam>
+  public abstract class BaseErrorValidator<T> : AbstractValidator<T>, IErrorValidator<T>, IValidator<T>
+  {
+    #region IErrorValidator
+
+    public ValidationResult Validate(T instance, bool throwOnErrors)
+    {
+      if (throwOnErrors)
+        return base.Validate(ValidationContext<T>.CreateWithOptions(instance, s => s.ThrowOnFailures()));
+      else
+        return base.Validate(instance);
+    }
+
+    public Task<ValidationResult> ValidateAsync(T instance, bool throwOnErrors, CancellationToken cancellation = default)
+    {
+      if (throwOnErrors)
+        return base.ValidateAsync(ValidationContext<T>.CreateWithOptions(instance, s => s.ThrowOnFailures()), cancellation);
+      else
+        return base.ValidateAsync(instance);
+    }
+
+    #endregion
+  }
+}
---
+++ Kernel/Domain/MicroSungero.Kernel.Domain/Validation/IErrorValidator.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FluentValidation.Results;
+
+namespace MicroSungero.Kernel.Domain.Validation
+{
+  /// <summary>
+  /// Object validator that can throw validation exceptions.
+  /// </summary>
+  /// <typeparam name="T">Type of objects that are validated by particular implementation of validator.</typeparam>
+  public interface IErrorValidator<T>
+  {
+    /// <summary>
+    /// Validate object.
+    /// </summary>
+    /// <param name="instance">Object instance.</param>
+    /// <param name="throwOnErrors">Should throw exception on validation error?</param>
+    /// <returns>Validation result.</returns>
+    ValidationResult Validate(T instance, bool throwOnErrors);
+
+    /// <summary>
+    /// Async validate object.
+    /// </summary>
+    /// <param name="instance">Object instance.</param>
+    /// <param name="throwOnErrors">Should throw exception on validation error?</param>
+    /// <param name="cancellation">Cancellation token.</param>
+    /// <returns>Validation result.</returns>
+    Task<ValidationResult> ValidateAsync(T instance, bool throwOnErrors, CancellationToken cancellation = default);
+  }
+}
