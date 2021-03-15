8d7d6881bb15af153312ca9f0cd3189896e16330
Шумков Артём (Shumkov_AV) <Shumkov_AV@directum.ru>
2021-03-16 00:30:17 +0400

    Base child entity implementation.

---
+++ Kernel/Domain/MicroSungero.Kernel.Domain/Entities/ChildEntity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MicroSungero.Kernel.Domain.Entities
+{
+  /// <summary>
+  /// Base child entity implementation.
+  /// </summary>
+  public abstract class ChildEntity : Entity, IChildEntity, IInternalChildEntity
+  {
+    #region Constants
+
+    /// <summary>
+    /// Base child entity type identifier.
+    /// </summary>
+    public static new readonly Guid ClassTypeGuid = Guid.Parse("78B71316-3F5E-484D-B53B-F7F98E85C93F");
+
+    #endregion
+
+    #region IChildEntity
+
+    public virtual IEntity RootEntity { get; set; }
+
+    #endregion
+
+    #region IEntity
+
+    public override Guid TypeGuid => ClassTypeGuid;
+
+    #endregion
+
+    #region Constructors
+
+    protected ChildEntity()
+    {
+    }
+
+    #endregion
+  }
+}
