10451518394a7623fb02cd1bad4db438e174f1e8
Шумков Артём (Shumkov_AV) <Shumkov_AV@directum.ru>
2021-03-20 00:25:07 +0400

    Remove unused dependencies

--- Kernel/API/MicroSungero.Kernel.API.Services/Command/CommandValidationService.cs
+++ Kernel/API/MicroSungero.Kernel.API.Services/Command/CommandValidationService.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using FluentValidation;
 using MicroSungero.Kernel.API.Validation;
 
 namespace MicroSungero.Kernel.API.Services
