84f5247eff330f5eabb3e449eacbd58738d00b21
Шумков Артём (Shumkov_AV) <Shumkov_AV@directum.ru>
2021-03-20 19:42:20 +0400

    SessionException - An exception occured working with database using the unit-of-work pattern implementation.

---
+++ Kernel/Data/MicroSungero.Data.Abstractions/Exceptions/SessionException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MicroSungero.Data.Exceptions
+{
+  /// <summary>
+  /// An exception occured working with database using the unit-of-work pattern implementation.
+  /// </summary>
+  public class SessionException : Exception
+  {
+    /// <summary>
+    /// Create session exception.
+    /// </summary>
+    public SessionException()
+    {
+    }
+
+    /// <summary>
+    /// Create session exception.
+    /// </summary>
+    /// <param name="message">Error message.</param>
+    public SessionException(string message)
+        : base(message)
+    {
+    }
+
+    /// <summary>
+    /// Create session exception.
+    /// </summary>
+    /// <param name="message">Error message.</param>
+    /// <param name="innerException">Original data access exception.</param>
+    public SessionException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+  }
+}
