b93ecedb955e08ebc9870dab2a0769fc2d68d3fe
Шумков Артём (Shumkov_AV) <Shumkov_AV@directum.ru>
2021-03-20 19:37:56 +0400

    Abstraction for database transaction

---
+++ Kernel/Data/MicroSungero.Data.Abstractions/ITransaction.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MicroSungero.Data
+{
+  /// <summary>
+  /// The database transaction.
+  /// </summary>
+  public interface ITransaction: IDisposable
+  {
+    /// <summary>
+    /// The transaction identifier.
+    /// </summary>
+    Guid TransactionId { get; }
+
+    /// <summary>
+    /// Commits all changes made to the database in the current transaction.
+    /// </summary>
+    void Commit();
+
+    /// <summary>
+    /// Discards all changes made to the database in the current transaction.
+    /// </summary>
+    void Rollback();
+
+    /// <summary>
+    /// Commits all changes made to the database in the current transaction asynchronously.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    Task CommitAsync(CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Discards all changes made to the database in the current transaction asynchronously.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    Task RollbackAsync(CancellationToken cancellationToken = default);
+  }
+}
