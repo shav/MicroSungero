77ff463d9e8c420538565979b4971289295b12cc
Шумков Артём (Shumkov_AV) <Shumkov_AV@directum.ru>
2021-03-20 19:38:21 +0400

    Abstraction for change tracking entry

---
+++ Kernel/Data/MicroSungero.Data.Abstractions/IRecordEntry.cs
@@ -0,0 +1,19 @@
+﻿namespace MicroSungero.Data
+{
+  /// <summary>
+  /// Change tracking entry for the persistent record.
+  /// </summary>
+  /// <typeparam name="TRecord"></typeparam>
+  public interface IRecordEntry<TRecord>
+  {
+    /// <summary>
+    /// Persistent record.
+    /// </summary>
+    TRecord Record { get; }
+
+    /// <summary>
+    /// Change tracking state of the record.
+    /// </summary>
+    RecordState State { get; set; }
+  }
+}
---
+++ Kernel/Data/MicroSungero.Data.Abstractions/RecordState.cs
@@ -0,0 +1,36 @@
+﻿namespace MicroSungero.Data
+{
+  /// <summary>
+  /// Change tracking state of the peristent object record.
+  /// </summary>
+  public enum RecordState
+  {
+    /// <summary>
+    /// The record is not being tracked by the context.
+    /// </summary>
+    Detached,
+
+    /// <summary>
+    /// The record is being tracked by the context and exists in the database.
+    /// Its property values have not changed from the values in the database.
+    /// </summary>
+    Unchanged,
+
+    /// <summary>
+    /// The record is being tracked by the context and exists in the database.
+    /// It has been marked for deletion from the database.
+    /// </summary>
+    Deleted,
+
+    /// <summary>
+    /// The record is being tracked by the context and exists in the database.
+    /// Some or all of its property values have been modified.
+    /// </summary>
+    Modified,
+
+    /// <summary>
+    /// The record is being tracked by the context but does not yet exist in the database.
+    /// </summary>
+    Added
+  }
+}
