09be62feeb500d86d401cbf1407cefe63d3ad40c
Шумков Артём (Shumkov_AV) <Shumkov_AV@directum.ru>
2021-03-20 18:59:27 +0400

    PersistentObject anstraction

---
+++ Kernel/Domain/MicroSungero.Kernel.Domain.Abstractions/IPersistentObject.cs
@@ -0,0 +1,18 @@
+﻿namespace MicroSungero.Kernel.Domain
+{
+  /// <summary>
+  /// Object that can be persisted to the storage.
+  /// </summary>
+  public interface IPersistentObject
+  {
+    /// <summary>
+    /// Object is new and not persisted to the storage yet.
+    /// </summary>
+    bool IsTransient { get; set; }
+
+    /// <summary>
+    /// Object is deleted from the storage.
+    /// </summary>
+    bool IsDeleted { get; set; }
+  }
+}
