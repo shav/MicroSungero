209bb1818849be74f921d30b4f14bdab1f9c4127
Шумков Артём (Shumkov_AV) <Shumkov_AV@directum.ru>
2021-03-21 15:04:46 +0400

    Unit-of-work provider

---
+++ Kernel/Data/MicroSungero.Data.Abstractions/Unit-of-work/IUnitOfWorkContext.cs
@@ -0,0 +1,18 @@
+﻿namespace MicroSungero.Data
+{
+  /// <summary>
+  /// Provider that gives access to unit-of-work context.
+  /// </summary>
+  public interface IUnitOfWorkContext
+  {
+    /// <summary>
+    /// Active unit of work.
+    /// </summary>
+    IUnitOfWork Current { get; }
+
+    /// <summary>
+    /// Factory that creates new unit-of-work instance.
+    /// </summary>
+    IUnitOfWorkFactory Factory { get; }
+  }
+}
