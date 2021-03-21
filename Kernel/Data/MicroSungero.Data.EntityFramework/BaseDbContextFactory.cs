ab5dc15c50b486ae1900f13ea763e2ab252a1eab
Шумков Артём (Shumkov_AV) <Shumkov_AV@directum.ru>
2021-03-21 19:36:38 +0400

    DbContextFactory stub

---
+++ Kernel/Data/MicroSungero.Data.EntityFramework/BaseDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace MicroSungero.Data.EntityFramework
+{
+  /// <summary>
+  /// Factory that creates base database context instance.
+  /// </summary>
+  public class BaseDbContextFactory: IDbContextFactory
+  {
+    #region IDbContextFactory
+
+    public IDbContext Create()
+    {
+      throw new System.NotImplementedException();
+    }
+
+    #endregion
+  }
+}
