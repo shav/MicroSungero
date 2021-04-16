f920cc9fbd94e8b0ad32f73fd104c9740a396ce8
Шумков Артём (Shumkov_AV) <Shumkov_AV@directum.ru>
2021-04-16 22:21:38 +0400

    Entity tag DTO.

---
+++ System/API/MicroSungero.System.API/Models/EntityTagDto.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using MicroSungero.System.Domain;
+
+namespace MicroSungero.System.API.Models
+{
+  /// <summary>
+  /// Entity tag DTO.
+  /// </summary>
+  [AutoMap(sourceType: typeof(EntityTag))]
+  public class EntityTagDto
+  {
+    /// <summary>
+    /// Tag name.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Tag color.
+    /// </summary>
+    public string Color { get; set; }
+  }
+}
