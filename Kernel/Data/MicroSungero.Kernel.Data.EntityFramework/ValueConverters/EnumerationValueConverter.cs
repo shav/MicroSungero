cc4f7d59164596d14e6beda02f3476eacb16af50
Шумков Артём (Shumkov_AV) <Shumkov_AV@directum.ru>
2021-04-17 02:35:57 +0400

    Value converter between Enumeration and its value stored in database.

---
+++ Kernel/Data/MicroSungero.Kernel.Data.EntityFramework/ValueConverters/EnumerationValueConverter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using MicroSungero.Kernel.Domain;
+
+namespace MicroSungero.Kernel.Data.EntityFramework
+{
+  /// <summary>
+  /// Value converter between Enumeration and its value stored in database.
+  /// </summary>
+  /// <typeparam name="TEnumeration">Type of enumeration.</typeparam>
+  public class EnumerationValueConverter<TEnumeration>: ValueConverter<TEnumeration, string>
+    where TEnumeration: Enumeration
+  {
+    /// <summary>
+    /// Create enumeration value converter.
+    /// </summary>
+    public EnumerationValueConverter()
+      : base(v => v.ToString(), s => Enumeration.Parse<TEnumeration>(s))
+    {
+    }
+  }
+}
