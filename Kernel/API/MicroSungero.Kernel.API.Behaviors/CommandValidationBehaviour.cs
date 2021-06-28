0f04b2ff02b06d4f388e8939e98af5b94321741b
Шумков Артём (Shumkov_AV) <Shumkov_AV@directum.ru>
2021-06-28 12:42:03 +0400

    Fixed bug: an error occured (generic argument type mismatch) when tried to apply CommandValidationBehaviour to queries

--- Kernel/API/MicroSungero.Kernel.API.Behaviors/CommandValidationBehaviour.cs
+++ Kernel/API/MicroSungero.Kernel.API.Behaviors/CommandValidationBehaviour.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,6 +15,50 @@ namespace MicroSungero.Kernel.API.Behaviors
   /// <typeparam name="TCommand">Type of command.</typeparam>
   /// <typeparam name="TResult">Type of command result.</typeparam>
   public class CommandValidationBehaviour<TCommand, TResult> : IPipelineBehavior<TCommand, TResult>
+  {
+    /// <summary>
+    /// Command validation service.
+    /// </summary>
+    private readonly ICommandValidationService validationService;
+
+    /// <summary>
+    /// All command validators.
+    /// </summary>
+    private readonly IValidator<TCommand>[] validators;
+
+    /// <summary>
+    /// Create command validation behavior.
+    /// </summary>
+    /// <param name="validationService">Command validation service.</param>
+    /// <param name="validators">Command validators.</param>
+    public CommandValidationBehaviour(ICommandValidationService validationService, IEnumerable<IValidator<TCommand>> validators)
+    {
+      this.validationService = validationService;
+      this.validators = validators.ToArray();
+    }
+
+    #region IPipelineBehavior
+
+    public async Task<TResult> Handle(TCommand command, CancellationToken cancellationToken, RequestHandlerDelegate<TResult> next)
+    {
+      if (command is ICommand<TResult>)
+      {
+        var behaviorType = typeof(CommandValidationBehaviourImpl<,>).MakeGenericType(typeof(TCommand), typeof(TResult));
+        var behavior = (IPipelineBehavior<TCommand, TResult>)Activator.CreateInstance(behaviorType, this.validationService, this.validators);
+        await behavior.Handle(command, cancellationToken, next);
+      }
+      return await next();
+    }
+
+    #endregion
+  }
+
+  /// <summary>
+  /// The implementation of command pipeline behavior for validation of command arguments.
+  /// </summary>
+  /// <typeparam name="TCommand">Type of command.</typeparam>
+  /// <typeparam name="TResult">Type of command result.</typeparam>
+  internal class CommandValidationBehaviourImpl<TCommand, TResult> : IPipelineBehavior<TCommand, TResult>
     where TCommand : ICommand<TResult>
   {
     /// <summary>
@@ -22,11 +67,11 @@ namespace MicroSungero.Kernel.API.Behaviors
     private readonly ICommandValidationService validationService;
 
     /// <summary>
-    /// Create command validation behavior.
+    /// Create implementation of command validation behavior.
     /// </summary>
     /// <param name="validationService">Command validation service.</param>
     /// <param name="validators">Command validators.</param>
-    public CommandValidationBehaviour(ICommandValidationService validationService, IEnumerable<IValidator<TCommand>> validators)
+    public CommandValidationBehaviourImpl(ICommandValidationService validationService, IEnumerable<IValidator<TCommand>> validators)
     {
       this.validationService = validationService;
       this.validationService.AddValidators(validators.OfType<ICommandValidator<TCommand>>().ToArray());
